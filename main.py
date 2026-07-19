e3bf4666c96d1c97ef245965558e2e00fea21f11
kawshashank <kawshashank@gmail.com>
2026-07-19 14:42:30 +0530

    updating to false

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Vijayshwar Jantri - Muhurat API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
